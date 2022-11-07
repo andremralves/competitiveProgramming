a78374804dffad32578dd4cace2b80113ed07117
Andre Alves <andremralves@gmail.com>
2022-11-07 09:38:31 -0300

    update gen file

--- generate.py
+++ generate.py
@@ -5,6 +5,4 @@ template ="/home/andre/repos/competitiveProgramming/template.cpp"
 for fileName in files:
     subprocess.run(["touch", fileName])
     subprocess.run(["cp", template, fileName])
-    #bashCommand = "touch " + fileName + " && cp " + template + " " + fileName
-    #process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-    #output, error = process.communicate()
+subprocess.run(["touch", "in.txt"])
