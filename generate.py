5a97270153613429d261c875de048e101c5420db
Andre Alves <andremralves@gmail.com>
2022-11-17 02:15:09 -0300

    update generate to create only if file do not exists

--- generate.py
+++ generate.py
@@ -1,8 +1,10 @@
 import subprocess
+from os.path import exists
 
 files = ["a.cpp", "b.cpp", "c.cpp", "d.cpp", "e.cpp", "f.cpp"]
 template ="/home/andre/repos/competitiveProgramming/template.cpp"
 for fileName in files:
-    subprocess.run(["touch", fileName])
-    subprocess.run(["cp", template, fileName])
+    if(not exists("./" + fileName)):
+        subprocess.run(["touch", fileName])
+        subprocess.run(["cp", template, fileName])
 subprocess.run(["touch", "in.txt"])
