b83ef4c1fff9a745db13bafb8c5d35c5d9faae64
Andre Alves <andremralves@gmail.com>
2023-04-02 14:59:11 -0300

    update template

--- template.cpp
+++ template.cpp
@@ -1,7 +1,11 @@
 #include <bits/stdc++.h>
 
+#define pb(x) push_back(x)
+
 using namespace std;
 using ll = int64_t;
+
+ll ceil(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
 
 void solve() {
 
