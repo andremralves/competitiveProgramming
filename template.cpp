48faa9745ea4c2a32db11d94fbb87a475c3af867
Andre Alves <andremralves@gmail.com>
2023-04-24 16:29:52 -0300

    update template

--- template.cpp
+++ template.cpp
@@ -1,9 +1,11 @@
 #include <bits/stdc++.h>
 
-using namespace std;
 #define pb(x) push_back(x)
 #define all(x) x.begin(),x.end()
+
+using namespace std;
 using ll = int64_t;
+
 ll ceil(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
 
 void solve() {
@@ -12,11 +14,8 @@ void solve() {
 
 int main ()
 {
-    int t;
+    int t = 1;
     cin>>t;
-    while (t--)
-    {
-        solve();    
-    }
+    while (t--) solve();
     return 0;
 }
