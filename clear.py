fbae3c2a3883071ee300e62fee9f40972b0c0b64
Andre Alves <andremralves@gmail.com>
2023-05-17 02:56:07 -0300

    add script to remove unused cpp files

---
+++ clear.py
@@ -0,0 +1,35 @@
+import os
+
+rootdir = '/home/andre/repos/competitiveProgramming'
+
+letters = "cdefghijklmnopqrstuvwxyz"
+files_to_search = set(map(lambda letter: letter + ".cpp", letters))
+files_to_remove = []
+
+
+for subdir, dirs, files in os.walk(rootdir):
+    if '.git' in subdir:
+        continue 
+
+    for file in files:
+        if file not in files_to_search:
+            continue
+
+        file_path = os.path.join(subdir, file)
+        file_size = os.path.getsize(file_path)
+
+        if file_size == 431:
+            files_to_remove.append(file_path)
+
+if len(files_to_remove) == 0:
+    print("No unused files to be removed.")
+else:
+    for file in files_to_remove:
+        file_size = os.path.getsize(file)
+        print(f'{file} {file_size}K')
+    print(f"Found {len(files_to_remove)} possible unused files. Remove them? (y/n):", end = " ")
+
+    confirm = input()
+    if(confirm.lower() == "y"):
+        for file in files_to_remove:
+            os.remove(file)
