82461557433380ac0cc43698dea3400bc152d338
Andre Alves <andremralves@gmail.com>
2023-06-30 21:40:02 -0300

    Cses minimizing coins

---
+++ cses/dp/minimizing_coins.cpp
@@ -0,0 +1,50 @@
+/**
+ *    author: mralves 
+ *    created: 30-06-2023 08:45:46       
+**/
+#include <bits/stdc++.h>
+
+#define pb(x) push_back(x)
+#define all(x) x.begin(),x.end()
+
+using namespace std;
+using ll = int64_t;
+using ii = pair<int, int>;
+
+ll ceil(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+vector<ii> dir4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+vector<ii> dir8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+void solve() {
+    int n, x;
+    cin>>n>>x;
+    vector<int> a(n);
+    for(int i=0; i<n; i++) {
+        cin>>a[i];
+    }
+
+    const int inf = 1e7;
+    vector<int> dp(x+1, inf);
+    dp[0] = 0;
+    for(int i=1; i<=x; i++) {
+        for(auto v:a) {
+            if(i-v >= 0) {
+                dp[i] = min(dp[i], dp[i-v] + 1);
+            }
+        }
+    }
+    if(dp[x] == inf)
+        cout<<"-1\n";
+    else
+        cout<<dp[x]<<"\n";
+}
+
+int main ()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t = 1;
+    while (t--) solve();
+    return 0;
+}
