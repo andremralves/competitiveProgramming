98be03034fd1009a1df4e6c643afd5576ec1dca4
Andre Alves <andremralves@gmail.com>
2023-09-01 11:23:20 -0300

    Codeforces eduRound 146

---
+++ codeforces/eduRound146/a.cpp
@@ -0,0 +1,40 @@
+/**
+ *    author: mralves 
+ *    created: 01-09-2023 00:51:41       
+**/
+#include <bits/stdc++.h>
+
+#define pb(x) push_back(x)
+#define all(x) x.begin(),x.end()
+
+using namespace std;
+using ll = int64_t;
+using ii = pair<int, int>;
+
+ll ceil(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+vector<ii> dir4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+vector<ii> dir8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+void solve() {
+    ll n, k;
+    cin>>n>>k;
+
+    ll y = n/k * k;
+
+    if(n%2 == 0 or (n-y)%2 == 0 or (n-y+k)%2==0) {
+        cout<<"YES\n";
+    } else {
+        cout<<"NO\n";
+    }
+}
+
+int main ()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t = 1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
+}
---
+++ codeforces/eduRound146/b.cpp
@@ -0,0 +1,66 @@
+/**
+ *    author: mralves 
+ *    created: 01-09-2023 00:51:41       
+**/
+#include <bits/stdc++.h>
+
+#define pb(x) push_back(x)
+#define all(x) x.begin(),x.end()
+
+using namespace std;
+using ll = int64_t;
+using ii = pair<int, int>;
+
+ll ceil(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
+vector<ii> dir4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+vector<ii> dir8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+ 
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
+
+void solve() {
+    int a, b;
+    cin>>a>>b;
+
+    int ans = 2e9;
+
+    for(int i=1; i<=1e5; i++) {
+        ans = min(ans, (a+i-1)/i + (b+i-1)/i + i-1);
+    }
+    cout<<ans<<"\n";
+}
+
+int main ()
+{
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+
+    int t = 1;
+    cin>>t;
+    while (t--) solve();
+    return 0;
+}
