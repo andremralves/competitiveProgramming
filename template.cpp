170972a9e755f0acde03b5006df4674cca4f64a7
Andre Alves <andremralves@gmail.com>
2023-10-25 02:54:49 -0300

    Update template

--- template.cpp
+++ template.cpp
@@ -5,11 +5,11 @@
 
 using namespace std;
 using ll = int64_t;
-using ii = pair<int, int>;
+using pii = pair<int, int>;
 
 ll ceil(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
-vector<ii> dir4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-vector<ii> dir8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+vector<pii> dir4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+vector<pii> dir8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
 
 void solve() {
 
