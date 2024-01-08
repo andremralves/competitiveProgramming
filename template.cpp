7097dbc7616373dfdfe81df1c77ca5f4f604f5e2
Andre Alves <andremralves@gmail.com>
2024-01-08 17:39:38 -0300

    Update template

--- template.cpp
+++ template.cpp
@@ -1,27 +1,21 @@
 #include <bits/stdc++.h>
 
-#define pb(x) push_back(x)
 #define all(x) x.begin(),x.end()
 
+// less is more
 using namespace std;
-using ll = int64_t;
-using pii = pair<int, int>;
-
-ll ceil(ll a, ll b) {return a % b == 0 ? a / b : a / b + 1;}
-vector<pii> dir4 = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-vector<pii> dir8 = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+using ll = long long;
 
 void solve() {
 
 }
 
-int main ()
-{
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
+int main () {
+  std::ios::sync_with_stdio(false);
+  std::cin.tie(nullptr);
 
-    int t = 1;
-    cin>>t;
-    while (t--) solve();
-    return 0;
+  int t = 1;
+  cin>>t;
+  while (t--) solve();
+  return 0;
 }
