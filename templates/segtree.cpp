27163d3acb07249febf93f46f734dc32ee79e0ec
Andre Alves <andremralves@gmail.com>
2024-11-07 06:37:49 -0300

    Update all

--- templates/segtree.cpp
+++ templates/segtree.cpp
@@ -1,35 +1,22 @@
-/**
- * Description: 1D point update and range query where \texttt{cmb} is
- 	* any associative operation. \texttt{seg[1]==query(0,N-1)}.
- * Time: O(\log N)
- * Source: 
-	* http://codeforces.com/blog/entry/18051
-	* KACTL
- * Verification: SPOJ Fenwick
- */
-
-template<class T> struct SegTree { // cmb(ID,b) = b
-	const T ID{}; T cmb(T a, T b) { return a+b; } 
-	int n; V<T> seg;
-	void init(int _n) { // upd, query also work if n = _n
-		for (n = 1; n < _n; ) n *= 2; 
-		seg.assign(2*n,ID); }
-	void pull(int p) { seg[p] = cmb(seg[2*p],seg[2*p+1]); }
-	void upd(int p, T val) { // set val at position p
-		seg[p += n] = val; for (p /= 2; p; p /= 2) pull(p); }
-	T query(int l, int r) {	// zero-indexed, inclusive
-		T ra = ID, rb = ID;
-		for (l += n, r += n+1; l < r; l /= 2, r /= 2) {
-			if (l&1) ra = cmb(ra,seg[l++]);
-			if (r&1) rb = cmb(seg[--r],rb);
-		}
-		return cmb(ra,rb);
-	}
-	/// int first_at_least(int lo, int val, int ind, int l, int r) { // if seg stores max across range
-	/// 	if (r < lo || val > seg[ind]) return -1;
-	/// 	if (l == r) return l;
-	/// 	int m = (l+r)/2;
-	/// 	int res = first_at_least(lo,val,2*ind,l,m); if (res != -1) return res;
-	/// 	return first_at_least(lo,val,2*ind+1,m+1,r);
-	/// }
-};
+ll SZ = 262144*2; //set this to power of two
+mi* seg = new mi[2*SZ]; //segtree implementation by bqi343's Github
+ 
+mi combine(mi a, mi b) {  
+    if (a.v == 0) return b;
+    return a;
+}
+ 
+ 
+void update(int p, mi value) {  
+    for (seg[p += SZ] += value; p > 1; p >>= 1)
+        seg[p>>1] = combine(seg[(p|1)^1], seg[p|1]);
+}
+ 
+mi query(int l, int r) {  // sum on interval [l, r]
+    mi resL = 0, resR = 0; r++;
+    for (l += SZ, r += SZ; l < r; l >>= 1, r >>= 1) {
+        if (l&1) resL = combine(resL,seg[l++]);
+        if (r&1) resR = combine(seg[--r],resR);
+    }
+    return combine(resL,resR);
+}
